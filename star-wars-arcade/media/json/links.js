4f58ed093626a4a7b1bcc90f0a3a218dcca955af
fgfiles <32440046+fgfiles@users.noreply.github.com>
2020-08-06 13:12:59 +0500

    Updated

---
+++ star-wars-arcade/media/json/links.js
@@ -0,0 +1,11 @@
+window.links = {
+	"ARCADE_URL":"#",
+  	"GAME1_URL": "https://play.forestrygames.com/yoda-jedi-training",
+  	"GAME2_URL": "https://play.forestrygames.com/star-wars-rebels-team-tactis",
+  	"GAME3_URL": "https://play.forestrygames.com/x-wing-fighter",
+
+  	"ARCADE_URL_MOBILE":"#",
+  	"GAME1_URL_MOBILE": "https://play.forestrygames.com/yoda-jedi-training/",
+  	"GAME2_URL_MOBILE": "https://play.forestrygames.com/star-wars-rebels-team-tactis/",
+  	"GAME3_URL_MOBILE": "https://play.forestrygames.com/x-wing-fighter/"
+}
